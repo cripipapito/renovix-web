93d420387aa809d70e1c8a272d565b316cb85274
cripipapito <migueplay3@gmail.com>
2025-03-02 19:30:58 -0300

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}" // Asegura que Tailwind procese los archivos dentro de "src"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
